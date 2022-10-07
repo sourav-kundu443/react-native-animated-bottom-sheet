22561972c6464a6668b4869c778c6360308aa4b2
sourav-kundu443 <kundusourav443@gmail.com>
2022-10-07 15:17:09 +0530

    with useEffect

---
+++ src/screens/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    height: 50,
+    backgroundColor: 'white',
+    aspectRatio: 1,
+    borderRadius: 25,
+    opacity: 0.8,
+  },
+});
+
+export default Styles;
